df64908b5f9e0dfc8794a53662a5bbc1656390b7
Talisson Trindade <framework26@localiza.com>
2021-09-29 13:58:41 -0300

    install commitlint/husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import DayPicker from 'react-day-picker';
+// Or import the input component
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
 import './App.css';
 
 function App() {
+  function handleDayChange(selectedDay: any, modifiers: any, dayPickerInput: any) {
+    console.log({ selectedDay, modifiers, dayPickerInput })
+    const input = dayPickerInput.getInput();
+    console.log({
+      input,
+      selectedDay,
+      isEmpty: !input.value.trim(),
+      isValidDay: typeof selectedDay !== 'undefined',
+      isDisabled: modifiers.disabled === true,
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <DayPickerInput
+          dayPickerProps={{
+            initialMonth: new Date(2021, 8),
+            disabledDays: [
+              {
+                after: new Date(2021, 1, 1),
+                before: new Date(2021, 1, 2),
+              },// desabilita range de datas antes e depois de determinada
+              new Date(2021, 8, 11),
+              new Date(2021, 8, 15), // desabilita dias especificos
+              { daysOfWeek: [6, 0] } // desabilita fds
+            ],
+          }}
+          onDayChange={handleDayChange}
+        />
+      </div>
     </div>
   );
 }
